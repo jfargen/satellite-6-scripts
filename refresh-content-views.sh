b213549602cb32222d557b84da92f2c34f71f0d7
Jamie Fargen <jamie@fargenable.com>
2021-07-30 12:05:53 -0400

    Create refresh-content-views.sh
    This script will synchronize a list of Satellite repositories and then publish a list of content views. Order is important any composite content views should be published at the end after the content views they are made of have already been published.

---
+++ refresh-content-views.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# This script will sync a list of repositories and then publish content using a list of content-views.
+# If this script is dropped into cron, it will result in activation-key(s) associated with content-views
+# being sync'd whenever the script is executed by crontab.
+
+repository_list=( 1 2 3 4 5 6 7 8 9 10 )
+contentview_list=( 20 21 22 23 )
+function synchronize_repos {
+for repository_id in "${repository_list[@]}"
+do
+      hammer repository sync --id=${repository_id}
+done
+}
+
+function publish_contentviews {
+
+for contentview_id in "${contentview_list[@]}"
+do
+      hammer content-view publish --id=${cvlist}
+done
+}
+
+
+# Execute functions
+# First synchronize the repositories
+synchronize_repos
+
+# Second publish the contentviews
+publish_contentviews
